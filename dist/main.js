bee0ef9b9f53bf233c67a0f0072cd09b68610acf
Amy <git@xvrqt.com>
2019-01-06 12:29:00 -0800

    Stopped tracking the dist/ folder. Will upload zipped artifacts instead.

--- dist/main.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=new Image,t=document.getElementsByTagName("html")[0];function a(e,n){n?e.classList.add("webp"):e.classList.add("no-webp")}n.onload=function(){var e=!!(0<n.height&&0<n.width);a(t,e)},n.onerror=function(){a(t,!1)},n.src="img/test_pixel.webp"}();
-//# sourceMappingURL=maps/main.js.map
